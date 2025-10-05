bebbdc05adcccc01e753d56d73245ab125dd1493
mdashad0 <ashad7666@gmail.com>
2025-10-05 21:56:28 +0530

    Update food delivery project: Fixed bugs, added order management, improved admin panel, enhanced frontend components

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './List.css'
+import axios from 'axios'
+import { toast } from "react-toastify";
 
-const List = () => {
+const List = ({url}) => {
+
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      }
+      else {
+        toast.error("Error fetching food list")
+      }
+    } catch (error) {
+      toast.error("Network error")
+      console.log(error);
+    }
+  }
+
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+      await fetchList();
+      if (response.data.success) {
+        toast.success(response.data.message)
+      } else {
+        toast.error("Error removing food item")
+      }
+    } catch (error) {
+      toast.error("Network error")
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, [])
+
   return (
     <div className='list add flex-col'>
       <p>All Foods List</p>
@@ -13,8 +52,18 @@ const List = () => {
           <b>Price</b>
           <b>Action</b>
         </div>
-        {/* Food items will be mapped here */}
-      </div> 
+        {list.map((item, index) => {
+          return (
+            <div key={index} className='list-table-format'>
+              <img src={`${url}/image/`+ item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className='cursor'>x</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express"
 import cors from "cors"
 import {connectDB} from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 // app config
@@ -19,6 +23,9 @@ connectDB();
 // api endpoints
 app.use("/api/food",foodRouter)
 app.use("/image",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get("/",(req,res) =>{
     res.send("API WORKING")
