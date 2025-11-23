fd57e8aca62e6a0728d0bd46c28964551a42106e
Md Ashad <asadanwarr0@gmail.com>
2025-11-24 01:10:32 +0530

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     destination: "uploads",
     filename:(req,file,cb) =>{
         return cb(null,`${Date.now()}${file.originalname}`)
-    }
+    };
 });
 
 const upload = multer ({storage:storage})
