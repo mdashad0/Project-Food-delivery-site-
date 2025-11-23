65ebc900c9bd66ec0fb487f6a0088af6902369e0
Md Ashad <asadanwarr0@gmail.com>
2025-11-24 01:11:10 +0530

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -16,7 +16,7 @@ const upload = multer ({storage:storage})
 
 
 foodRouter.post("/add", upload.single("image"),addFood);
-foodRouter.get("/list",listFood)
+foodRouter.get("/list",listFood);
 foodRouter.post("/remove",removeFood);
 
 
