ffa097a7c7058b9d68b857996cf84c57859453eb
Md Ashad <asadanwarr0@gmail.com>
2025-11-24 01:09:53 +0530

    Update cartRoute.js

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -7,4 +7,4 @@ cartRouter.post("/add",authMiddleware, addToCart);
 cartRouter.post("/remove",authMiddleware,  removeFromCart);
 cartRouter.post("/get", authMiddleware, getCart);
 
-export default cartRouter;authMiddleware 
+export default cartRouter;authMiddleware;
