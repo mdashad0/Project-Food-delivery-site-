64a8e54cc21e0be92f65e08f81f4c04293162f39
Md Ashad <asadanwarr0@gmail.com>
2026-01-03 20:33:26 +0530

    Fix cart item addition logic in cartController

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -8,17 +8,16 @@ const addToCart = async(req,res)=>{
         if (!cartData[req.body.itemId])
         {
             cartData[req.body.itemId] = 1 
-        };
+        }
         else{
             cartData[req.body.itemId] +=1;
-        };
+        }
         await userModel.findByIdAndUpdate(req.body.userId,{cartData});
         res.json({success:true,message:"Added To Cart"});
     } catch (error) {
         console.log(error);
         res.json({success:false,message:"Error"})
-    }
-
+    };
 }
 
 
